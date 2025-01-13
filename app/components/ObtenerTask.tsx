520814d7ee9b010b12a2713b3f40b087b2e658f1
Wilmer sneider <wil788ruiz@gmail.com>
2025-01-13 18:43:11 -0500

    haciendo peticiones

--- app/components/ObtenerTask.tsx
+++ app/components/ObtenerTask.tsx
@@ -1,25 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { fetchGetTask } from "../helpers/apis";
-
 export default function ObtenerTask() {
-  const [task, setTask] = useState([]);
-
-  useEffect(() => {
-    async function getTask() {
-      const data = await fetchGetTask();
-      setTask(data);
-    }
-
-    getTask();
-  }, []);
-
-  return (
-    <div>
-      {task.map((item) => (
-        <li>{JSON.stringify(item)}</li>
-      ))}
-    </div>
-  );
+  return <div>nada</div>;
 }
