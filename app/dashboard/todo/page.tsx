91ec98956f0171f742e87f3e92c80602feb6f69a
Wilmer sneider <wil788ruiz@gmail.com>
2025-01-13 19:26:21 -0500

    cambios

--- app/dashboard/todo/page.tsx
+++ app/dashboard/todo/page.tsx
@@ -1,9 +1,11 @@
 export default function TodoPage() {
   const nombre = "wilmer";
+  const apellido = "ramirez";
 
   return (
     <div>
       <h1>{nombre}</h1>
+      <p>{apellido}</p>
     </div>
   );
 }
