44228cab3595bbc2f012d1f1133b03f3e0689c7a
Wilmer sneider <wil788ruiz@gmail.com>
2025-01-13 19:28:06 -0500

    peticiones en la nueva rama

--- app/dashboard/todo/page.tsx
+++ app/dashboard/todo/page.tsx
@@ -1,11 +1,9 @@
 export default function TodoPage() {
   const nombre = "wilmer";
-  const apellido = "ramirez";
 
   return (
     <div>
       <h1>{nombre}</h1>
-      <p>{apellido}</p>
     </div>
   );
 }
