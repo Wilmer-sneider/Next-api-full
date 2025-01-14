0afed228c1af38fa8a52d25ec23eaf83a515f4cd
Wilmer sneider <wil788ruiz@gmail.com>
2025-01-13 19:20:35 -0500

    haciendo peticiones

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import ObtenerTask from "./components/ObtenerTask";
 export default function HomePage() {
   return (
     <>
-      <h2>hola</h2>
       <ObtenerTask />
-      <h2>desde actual</h2>
     </>
   );
 }
